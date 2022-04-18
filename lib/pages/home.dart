8bb05e526d6d1c04d50f7a50a212133e8d8332a9
Pasha Bolokhov <pasha.bolokhov@gmail.com>
2022-04-18 09:41:09 -0700

    feat: add bi–colour background layer to the home page

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -6,124 +6,130 @@ class HomePage extends StatelessWidget {
     final double screenHeight = MediaQuery.of(context).size.height;
     return Container(
       constraints: BoxConstraints.expand(),
-      decoration: const BoxDecoration(
-        image: DecorationImage(
-          image: AssetImage("assets/images/cord.png"),
-          fit: BoxFit.cover,
+      child: Stack(children: [
+        Container(
+          constraints: BoxConstraints.expand(),
+          child: Row(
+            children: <Widget>[
+              Expanded(flex: 1, child: Container(color: Colors.red)),
+              Expanded(flex: 1, child: Container(color: Colors.black)),
+            ],
+          ),
         ),
-      ),
-      child: Stack(
-        children: <Widget>[
-          Positioned(
-            top: 0,
-            bottom: screenHeight * 0.5,
-            left: 0,
-            right: 0,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  "Aux",
-                  style: TextStyle(
-                      color: Colors.purple,
-                      fontSize: 80,
-                      fontWeight: FontWeight.bold),
-                ),
-              ],
-            ),
-          ),
-          Positioned(
-            top: screenHeight * 0.2,
-            bottom: screenHeight * 0.2,
-            left: 0,
-            right: 0,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  "Battles",
-                  style: TextStyle(
-                      color: Colors.purple,
-                      fontSize: 80,
-                      fontWeight: FontWeight.bold),
-                ),
-              ],
-            ),
-          ),
-          Positioned(
-            top: screenHeight * 0.64,
-            bottom: screenHeight * 0.26,
-            left: 0,
-            right: 0,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ToggleButtons(
-                  color: Colors.black.withOpacity(0.60),
-                  selectedColor: Color(0xFF6200EE),
-                  selectedBorderColor: Color(0xFF6200EE),
-                  fillColor: Color(0xFF6200EE).withOpacity(0.08),
-                  splashColor: Color(0xFF6200EE).withOpacity(0.12),
-                  hoverColor: Color(0xFF6200EE).withOpacity(0.04),
-                  borderRadius: BorderRadius.circular(4.0),
-                  constraints: BoxConstraints(minHeight: 36.0),
-                  isSelected: <bool>[false, false],
-                  onPressed: (index) {
-                    // Respond to button selection
-                  },
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 16.0),
-                      child: Text(
-                        "Join",
-                        style: TextStyle(
-                            color: Colors.purple,
-                            fontSize: 50,
-                            fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 16.0),
-                      child: Text(
-                        "Host",
-                        style: TextStyle(
-                            color: Colors.purple,
-                            fontSize: 50,
-                            fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                  ],
-                )
-              ],
-            ),
-          ),
-          Positioned(
-            top: screenHeight * 0.7,
-            bottom: screenHeight * 0,
-            left: 0,
-            right: 0,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ElevatedButton(
-                  style: ButtonStyle(
-                    backgroundColor:
-                        MaterialStateProperty.all<Color>(Colors.black),
-                  ),
-                  onPressed: () {},
-                  child: Text(
-                    "Random",
+        Image.asset("assets/images/cord.png", fit: BoxFit.cover),
+        Stack(
+          children: <Widget>[
+            Positioned(
+              top: 0,
+              bottom: screenHeight * 0.5,
+              left: 0,
+              right: 0,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Aux",
                     style: TextStyle(
-                        color: Colors.red,
-                        fontSize: 50,
+                        color: Colors.purple,
+                        fontSize: 80,
                         fontWeight: FontWeight.bold),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-        ],
-      ),
+            Positioned(
+              top: screenHeight * 0.2,
+              bottom: screenHeight * 0.2,
+              left: 0,
+              right: 0,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Battles",
+                    style: TextStyle(
+                        color: Colors.purple,
+                        fontSize: 80,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+            ),
+            Positioned(
+              top: screenHeight * 0.64,
+              bottom: screenHeight * 0.26,
+              left: 0,
+              right: 0,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ToggleButtons(
+                    color: Colors.black.withOpacity(0.60),
+                    selectedColor: Color(0xFF6200EE),
+                    selectedBorderColor: Color(0xFF6200EE),
+                    fillColor: Color(0xFF6200EE).withOpacity(0.08),
+                    splashColor: Color(0xFF6200EE).withOpacity(0.12),
+                    hoverColor: Color(0xFF6200EE).withOpacity(0.04),
+                    borderRadius: BorderRadius.circular(4.0),
+                    constraints: BoxConstraints(minHeight: 36.0),
+                    isSelected: <bool>[false, false],
+                    onPressed: (index) {
+                      // Respond to button selection
+                    },
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 16.0),
+                        child: Text(
+                          "Join",
+                          style: TextStyle(
+                              color: Colors.purple,
+                              fontSize: 50,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 16.0),
+                        child: Text(
+                          "Host",
+                          style: TextStyle(
+                              color: Colors.purple,
+                              fontSize: 50,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+            Positioned(
+              top: screenHeight * 0.7,
+              bottom: screenHeight * 0,
+              left: 0,
+              right: 0,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all<Color>(Colors.black),
+                    ),
+                    onPressed: () {},
+                    child: Text(
+                      "Random",
+                      style: TextStyle(
+                          color: Colors.red,
+                          fontSize: 50,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ]),
     );
   }
 }
